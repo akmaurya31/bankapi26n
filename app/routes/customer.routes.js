4418cabfa7ba3d959165eda47d48e74a4aa9eeaa
Avinash Maurya <akmaurya31@gmail.com>
2020-11-26 15:17:15 +0530

    Update customer.routes.js

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -2,15 +2,7 @@ module.exports = app => {
   const customers = require("../controllers/customer.controller.js");
  
     // By Ashish Ji 
-    app.post("/addBankDetail", customers.addBankDetail);
-    app.post("/bankDetails", customers.showDetails);   //customers ->contrller, showDetails->method
-    app.post("/productApi", customers.findAllProducts);
-    app.post("/bankverify",customers.bankverify);
-    app.post("/bankverify2",customers.bankverify2);
-    app.get("/users/:emailId", customers.findOne1users);
-    app.get("/getNSEBank", customers.getnsebank);
-    app.post("/readFatca1",customers.readFatca1_nov);
-    app.get("/perchase",customers.perchase);//executed with error
-  app.post("/changePbank",customers.changePbank);
-    app.post("/deletebank",customers.deletebank);
+   
+    app.post("/mandate",customers.mandate);
+    app.get("/getUserDetails",customers.getUserDetails);
 };
